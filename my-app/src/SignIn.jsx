2f62db7c3ae7ec93283197e5f8f51d2cd162324e
TheGentleDucky <owenscruzsean@gmail.com>
2025-03-21 19:13:05 +0100

    Sign in page added.

---
+++ my-app/src/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from "./assets/sign-in.jsx";
+
+function App() {
+    return (
+        <div>
+            <SignIn />
+        </div>
+    );
+}
+
+export default App;
