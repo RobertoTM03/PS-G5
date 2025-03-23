8ff723a18b8312734c235e51669a19116eab07cb
Gabriel G.B. <gabrielgarciabuey@gmail.com>
2025-03-23 22:32:59 +0000

    build(server): Añadir nodemailer a package.json

--- server/express/src/auth/auth.routes.js
+++ server/express/src/auth/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, forgotPassword, resetPassword } = require('../auth/authController');
+const { register, login, forgotPassword, resetPassword } = require('./authController');
 
 /**
  * @openapi
