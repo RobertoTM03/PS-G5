97f2601564a822ea8cc7e9133c0f80cb95910d3a
TheGentleDucky <owenscruzsean@gmail.com>
2025-03-24 16:14:44 +0100

    Añadida la pagina de vista de grupos estática.

---
+++ my-app/src/VistaGrupos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VistaGrupos from "./assets/vistaGrupos.jsx";
+
+function App() {
+    return (
+        <div>
+            <VistaGrupos />
+        </div>
+    );
+}
+
+export default App;
