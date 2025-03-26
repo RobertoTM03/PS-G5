a585d5f58c099d82c2ae17d438055a492525d0d0
mariiarode <rodriguezdenizmaria@gmail.com>
2025-03-26 15:24:01 +0000

    Cambiada la vista de detalles de grupo

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,12 +7,13 @@ import IniciarSesion from './assets/signIn.jsx';
 import { PasswordRecover } from './assets/passwordRecover.jsx';
 import ResetearContraseña from "./assets/ResetearContraseña.jsx";
 import GroupAdminView from './assets/GroupAdminView.jsx';
+import VistaGrupos from './assets/vistaGrupos.jsx';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Inicio />} />
+                <Route path="/" element={<GroupAdminView />} />
                 <Route path="/Registrarse" element={<Registrarse />} />
                 <Route path="/IniciarSesion" element={<IniciarSesion />} />
                 <Route path="/PasswordRecover" element={<PasswordRecover />} />
