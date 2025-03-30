e3815217353573984fdc0ad4ce7c59021ffd3a9a
Gabriel G.B. <gabrielgarciabuey@gmail.com>
2025-03-30 17:02:35 +0100

    fix(react): Permite hot reload de React desplegado en Docker

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    strictPort: true,
+    hmr: {
+      clientPort: 80, // Ensures WebSocket connects to the correct port
+    },
+    watch: {
+      usePolling: true, // Helps with file change detection in Docker
+    },
+  },
 })
