5e937abba25884221f907bccea48c76ebead214e
Roberto Tejero Martín <robertotejeromartin@gmail.com>
2025-04-28 16:24:44 +0100

    feat(activities): Añadida tabla para actividades y participantes

--- server/database/init.sql
+++ server/database/init.sql
@@ -1,17 +1,15 @@
--- noinspection SqlNoDataSourceInspectionForFile
-
 CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       name VARCHAR(100) UNIQUE NOT NULL,
-                       email VARCHAR(100) UNIQUE NOT NULL,
-                       firebase_uid VARCHAR(100) UNIQUE NOT NULL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    firebase_uid VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE groups (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            description VARCHAR(100),
-            user_owner_id SERIAL NOT NULL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(100),
+    user_owner_id SERIAL NOT NULL
 );
 
 CREATE TABLE group_users (
@@ -23,35 +21,53 @@ CREATE TABLE group_users (
     UNIQUE (group_id, user_id)
 );
 
+CREATE TABLE activities (
+    id SERIAL PRIMARY KEY,
+    group_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    location VARCHAR(255),
+    created_by INT NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
+    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE SET NULL
+);
 
--- Insertando usuarios
+CREATE TABLE activity_participants (
+    id SERIAL PRIMARY KEY,
+    activity_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (activity_id) REFERENCES activities(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    UNIQUE (activity_id, user_id)
+);
+
 INSERT INTO users (name, email, firebase_uid) VALUES
     ('Alice', 'alice@example.com', 'ZhTfziUyocUVKV5UfDOQoW7AzPn2'),
     ('Bob', 'bob@example.com', 'zuHd4X3cMvcOKHzjn0tFUmrTH6H2'),
     ('Carlos', 'carlos@example.com', 'VkYSFA88o4XGeno6xKf4xgnGhDn2'),
     ('Diana', 'diana@example.com', 'RxS2gngetFOosWvhZeyz4v0UuSR2');
 
--- Insertando grupos
 INSERT INTO groups (name, description, user_owner_id) VALUES
     ('Grupo de Lectura', 'Amantes de los libros', 1),
     ('Gamers', 'Grupo de videojuegos', 2),
     ('Cine Club', 'Amantes del cine', 3);
 
--- Insertando relaciones usuarios-grupos
 INSERT INTO group_users (group_id, user_id) VALUES
-    -- Grupo de Lectura
+    -- Group 1
     (1, 1),  -- Alice
     (1, 2),  -- Bob
     (1, 3),  -- Carlos
     (1, 4),  -- Diana
 
-    -- Gamers Unidos
+    -- Group 2
     (2, 2),  -- Bob
     (2, 1),  -- Alice
     (2, 3),  -- Carlos
     (2, 4),  -- Diana
 
-    -- Cine Club
+    -- Group 3
     (3, 3),  -- Carlos
     (3, 4),  -- Diana
     (3, 1);  -- Alice
