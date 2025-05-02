07c57c2c39892439228e77823ed76fa28218c4a1
Gabriel G.B. <gabrielgarciabuey@gmail.com>
2025-05-02 17:15:06 +0100

    docs(express.expenses): Añade endpoints para editar, eliminar, contribuir a un gasto y eliminar contribución

--- server/express/src/expenses/expenses.routes.js
+++ server/express/src/expenses/expenses.routes.js
@@ -44,6 +44,17 @@ const router = express.Router();
  *                     format: float
  *                     description: The expense amount that has been covered or is to be covered. By default the currency is Euros.
  *                     example: 270.00
+ *                   author:
+ *                     type: object
+ *                     properties:
+ *                         userId:
+ *                             type: string
+ *                             description: ID of the User that created the expense
+ *                             example: "64ac9b7b5f8e2c001fc45abc"
+ *                         name:
+ *                             type: string
+ *                             description: Name of the expense author
+ *                             example: "John Doe"
  *                   contributor:
  *                     type: object
  *                     properties:
@@ -138,6 +149,17 @@ const router = express.Router();
  *                   format: float
  *                   description: The expense amount that has been covered or is to be covered. By default the currency is Euros.
  *                   example: 270.00
+ *                 author:
+ *                   type: object
+ *                   properties:
+ *                       userId:
+ *                           type: string
+ *                           description: ID of the User that created the expense
+ *                           example: "64ac9b7b5f8e2c001fc45abc"
+ *                       name:
+ *                           type: string
+ *                           description: Name of the expense author
+ *                           example: "John Doe"
  *                 contributor:
  *                   type: object
  *                   properties:
@@ -162,5 +184,228 @@ const router = express.Router();
  *       500:
  *         description: Something went wrong on our side
  */
+
+
+/**
+ * @openapi
+ * /groups/{groupId}/expenses/{expenseId}:
+ *   post:
+ *     summary: Edit expense
+ *     security:
+ *       - bearerAuth: []
+ *     description: Edits an expense from a given Group expense list
+ *     tags:
+ *       - Expenses
+ *     parameters:
+ *       - in: path
+ *         name: groupId
+ *         required: true
+ *         description: ID of the Group
+ *         schema:
+ *           type: string
+ *           example: "64ac9b7b5f8e2c001fc45def"
+ *       - in: path
+ *         name: expenseId
+ *         required: true
+ *         description: ID of the Expense to edit
+ *         schema:
+ *           type: string
+ *           example: "64ac9b7b5f8e2c001fc45def"
+ *     requestBody:
+ *         required: true
+ *         content:
+ *             application/json:
+ *                 schema:
+ *                     type: object
+ *                     properties:
+ *                       name:
+ *                         type: string
+ *                         example: "Plane tickets"
+ *                       amount:
+ *                         type: number
+ *                         format: float
+ *                         description: The expense amount that has been covered or is to be covered. By default the currency is Euros.
+ *                         example: 270.00
+ *                       contributorID:
+ *                         type: string
+ *                         description: ID of the User that covered the expense
+ *                         example: "64ac9b7b5f8e2c001fc45abc"
+ *                       tags:
+ *                         type: array
+ *                         items:
+ *                             type: string
+ *                             description: Tags associated to the expense
+ *                             example: "Trip to Mallorca"
+ *     responses:
+ *       200:
+ *         description: The expense was successfully modified. The resulting expense is sent alongside the response body.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               required: 
+ *                   - name
+ *                   - amount
+ *               properties:
+ *                 expenseId:
+ *                   type: string
+ *                   example: "53asc9b7b5fasduq1fc45abc"
+ *                 name:
+ *                   type: string
+ *                   example: "Plane tickets"
+ *                 amount:
+ *                   type: number
+ *                   format: float
+ *                   description: The expense amount that has been covered or is to be covered. By default the currency is Euros.
+ *                   example: 270.00
+ *                 author:
+ *                   type: object
+ *                   properties:
+ *                       userId:
+ *                           type: string
+ *                           description: ID of the User that created the expense
+ *                           example: "64ac9b7b5f8e2c001fc45abc"
+ *                       name:
+ *                           type: string
+ *                           description: Name of the expense author
+ *                           example: "John Doe" 
+ *                 contributor:
+ *                   type: object
+ *                   properties:
+ *                       userId:
+ *                           type: string
+ *                           description: ID of the User that covered the expense
+ *                           example: "64ac9b7b5f8e2c001fc45abc"
+ *                       name:
+ *                           type: string
+ *                           description: Name of the expense contributor
+ *                           example: "John Doe"
+ *                 tags:
+ *                   type: array
+ *                   items:
+ *                       type: string
+ *                       description: Tags associated to the expense
+ *                       example: "Trip to Mallorca"
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Group or Expense not found
+ *       500:
+ *         description: Something went wrong on our side
+ */
+
+/**
+ * @openapi
+ * /groups/{groupId}/expenses/{expenseId}:
+ *   delete:
+ *     summary: Delete expense
+ *     security:
+ *       - bearerAuth: []
+ *     description: Deletes an expense from a given Group expense list. Must be Group Owner to do so.
+ *     tags:
+ *       - Expenses
+ *     parameters:
+ *       - in: path
+ *         name: groupId
+ *         required: true
+ *         description: ID of the Group
+ *         schema:
+ *           type: string
+ *           example: "64ac9b7b5f8e2c001fc45def"
+ *       - in: path
+ *         name: expenseId
+ *         required: true
+ *         description: ID of the Expense to edit
+ *         schema:
+ *           type: string
+ *           example: "64ac9b7b5f8e2c001fc45def"
+ *     responses:
+ *       200:
+ *         description: The expense was deleted successfully.
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Group or Expense not found
+ *       500:
+ *         description: Something went wrong on our side
+ */
+
+
+/**
+ * @openapi
+ * /groups/{groupId}/expenses/{expenseId}/contribute:
+ *   post:
+ *     summary: Contribute to a Expense
+ *     security:
+ *       - bearerAuth: []
+ *     description: Adds the logged user as an uncovered Expense contributor.
+ *     tags:
+ *       - Expenses
+ *     parameters:
+ *       - in: path
+ *         name: groupId
+ *         required: true
+ *         description: ID of the Group
+ *         schema:
+ *           type: string
+ *           example: "64ac9b7b5f8e2c001fc45def"
+ *       - in: path
+ *         name: expenseId
+ *         required: true
+ *         description: ID of the Expense to edit
+ *         schema:
+ *           type: string
+ *           example: "64ac9b7b5f8e2c001fc45def"
+ *     responses:
+ *       200:
+ *         description: Contribution success.
+ *       401:
+ *         description: Expense already has a contributor
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Group or Expense not found
+ *       500:
+ *         description: Something went wrong on our side
+ */
+
+/**
+ * @openapi
+ * /groups/{groupId}/expenses/{expenseId}/remove-contribution:
+ *   post:
+ *     summary: Remove contribution
+ *     security:
+ *       - bearerAuth: []
+ *     description: Removes the logged User status as a contributor of a Expense.
+ *     tags:
+ *       - Expenses
+ *     parameters:
+ *       - in: path
+ *         name: groupId
+ *         required: true
+ *         description: ID of the Group
+ *         schema:
+ *           type: string
+ *           example: "64ac9b7b5f8e2c001fc45def"
+ *       - in: path
+ *         name: expenseId
+ *         required: true
+ *         description: ID of the Expense to edit
+ *         schema:
+ *           type: string
+ *           example: "64ac9b7b5f8e2c001fc45def"
+ *     responses:
+ *       200:
+ *         description: Contribution removed successfully
+ *       401:
+ *         description: Expense doesn't have a contributor yet
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Group or Expense not found
+ *       500:
+ *         description: Something went wrong on our side
+ */
+
 
 module.exports = router;
